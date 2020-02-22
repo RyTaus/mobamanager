51a17eaaa0beb813fe3ab88b35a1f4e3df499adc
ianlizzo <ianliz22@icloud.com>
2020-02-22 14:14:45 -0800

    Added some cool stuff
    Co-authored-by: Maddie Louis <mlouis2@lion.lmu.edu>

--- client/App.tsx
+++ client/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <LinearGradient
+      colors={["#67B26F", "#4ca2cd"]}
+      style={{
+        alignItems: "center",
+        borderRadius: 5,
+        flex: 1,
+        justifyContent: "center"
+      }}
+    >
+      <Text style={{ fontWeight: "900", fontSize: 69, color: "white" }}>
+        Moba Manager.
+      </Text>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
