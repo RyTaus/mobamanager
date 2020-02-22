bc02ce2afd50c841224ecd8f7cdb844ffcd1a175
ianlizzo <ianliz22@icloud.com>
2020-02-22 15:51:33 -0800

    Added navigation

---
+++ client/components/LoadingView/LoadingScreen.styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  gradient: {
+    alignItems: "center",
+    borderRadius: 5,
+    flex: 1,
+    justifyContent: "center"
+  },
+  header: {
+    fontWeight: "900",
+    fontSize: 69,
+    color: "white"
+  }
+});
---
+++ client/components/PlayerView/PlayerScreen.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+import { SearchBar } from "react-native-elements";
+
+export default function PlayerScreen() {
+  const [input, setInput] = useState("");
+  return (
+    <SafeAreaView>
+      <SearchBar
+        placeholder="Type here..."
+        onChangeText={input => setInput(input)}
+        value={input}
+        lightTheme
+      />
+    </SafeAreaView>
+  );
+}
+
+PlayerScreen.navigationOptions = { title: "Players", headerShown: true };
