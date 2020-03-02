14107de0200b91ea3eaeff4029f4b3385207512b
kmetelus <kevin.metelus@gmail.com>
2020-03-01 21:41:41 -0800

    begin endpoint for /user

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 
 	"github.com/RyTaus/mobamanager/server/graph/model"
@@ -10,30 +11,46 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// THE MAIN METHOD
+// type Config struct {
+// 	HttpPort     string
+// 	DatabasePath string
+// }
 
-func main() {
+// func NewConfig() *Config {
+// 	return &Config{
+// 		HttpPort:     ":8080",
+// 		DatabasePath: "moba:manager@tcp(keckmysql-rds.lmucs.com:3306)/moba_master",
+// 	}
+// }
+
+// func connectDatabase(config *Config) (*sql.DB, error) {
+// 	return sql.Open("mysql", config.DatabasePath)
+// }
+
+func getUsers(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
 	db, err := sql.Open("mysql", "moba:manager@tcp(keckmysql-rds.lmucs.com:3306)/moba_master")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
 	defer db.Close()
-
 	results, err := db.Query("SELECT id, username, password FROM `user`")
-
+	var users []model.User
 	for results.Next() {
 		var user model.User
 		err = results.Scan(&user.ID, &user.Username, &user.Password)
 		if err != nil {
 			panic(err.Error())
 		}
-		log.Printf(user.Username)
-		log.Printf(user.Password)
+		users = append(users, user)
 	}
+
+	json.NewEncoder(rw).Encode(users)
+
+}
+
+// THE MAIN METHOD
+
+func main() {
 	router := mux.NewRouter()
-	// router.HandleFunc("/user", getUser).Methods("GET")
+	router.HandleFunc("/user", getUsers).Methods("GET")
 	// router.HandleFunc("/user/{id}", getUserById).Methods("GET")
 	// router.HandleFunc("/user", createUser).Methods("POST")
 	// router.HandleFunc("/login", login).Methods("GET")
