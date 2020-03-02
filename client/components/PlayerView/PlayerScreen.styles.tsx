1ce7a71b3c9e77d09a7d4f189a7045be3d91dfc9
tedzchu <tedchu4@gmail.com>
2020-03-01 22:07:11 -0800

    Remove alignItems, padding

--- client/components/PlayerView/PlayerScreen.styles.tsx
+++ client/components/PlayerView/PlayerScreen.styles.tsx
@@ -1,10 +1,8 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   playerList: {
-    alignItems: "center",
     justifyContent: "center",
-    paddingTop: 10,
     //TODO: This padding bottom is problematic. Find a better way!!
     paddingBottom: 120
   }
