bb36ac3070c2f7e92bb77f391663f457940e6aa2
tedzchu <tedchu4@gmail.com>
2020-03-02 00:05:53 -0800

    Update SignupScreen to have similar buttons

--- client/components/SignupView/SignupScreen.tsx
+++ client/components/SignupView/SignupScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Input } from "react-native-elements";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
@@ -13,7 +13,16 @@ export default function SignupScreen() {
         <Input placeholder="Email" />
         <Input placeholder="Username" />
         <Input placeholder="Password" secureTextEntry={true} />
+        <Input placeholder="Repeat Password" secureTextEntry={true} />
         <Button title="Sign Up" onPress={() => navigation.navigate("Login")} />
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <TouchableOpacity>
+            <Text>Subscribe to Newsletter</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
