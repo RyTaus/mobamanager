ad61fc0d8afe3a190471d597f55e30211f2c545c
Maddie Louis <mlouis2@lion.lmu.edu>
2020-03-01 18:44:32 -0800

    started skeleton for detailed match view

---
+++ client/components/MatchesView/DetailedMatchView.styles.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { UPCOMING_MATCH_BORDER, CURRENT_MATCH_BORDER } from "../shared/Styles";
+
+export const styles = StyleSheet.create({
+  EditableView: {},
+  NonEditableView: {},
+  Player: {}
+});
--- client/components/MatchesView/DetailedMatchView.tsx
+++ client/components/MatchesView/DetailedMatchView.tsx
@@ -1,9 +1,71 @@
-import { View } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
-// import { styles } from "./DetailedMatchView.styles";
+import { styles } from "./DetailedMatchView.styles";
 
 import React from "react";
 
-export default function MatchesScreen(navigation) {
-  return <View></View>;
+export default function MatchesScreen({ date, editable }) {
+  if (editable) {
+    return <EditableView />;
+  } else {
+    return <NonEditableView />;
+  }
+}
+
+function EditableView() {
+  return (
+    <View style={styles.EditableView}>
+      <Players />
+    </View>
+  );
+}
+
+function NonEditableView() {
+  return (
+    <View style={styles.NonEditableView}>
+      <Players />
+    </View>
+  );
+}
+
+const PLAYER_TYPES = [
+  {
+    position: "Top",
+    playOptions: ["Work hard!!!", "Chill"]
+  },
+  {
+    position: "Bottom",
+    playOptions: ["Work hard!!!", "Chill"]
+  },
+  {
+    position: "Middle",
+    playOptions: ["Work hard!!!", "Chill"]
+  },
+  {
+    position: "AD/Carry",
+    playOptions: ["Work hard!!!", "Chill"]
+  },
+  {
+    position: "Support",
+    playOptions: ["Work hard!!!", "Chill"]
+  }
+];
+
+function Players() {
+  return (
+    <FlatList
+      data={PLAYER_TYPES}
+      renderItem={({ item }) => {
+        return Player(item);
+      }}
+    />
+  );
+}
+
+function Player({ position, playOptions }) {
+  return (
+    <View style={styles.Player}>
+      <Text>{position}</Text>
+    </View>
+  );
 }
