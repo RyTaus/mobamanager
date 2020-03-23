df5c677fae8f73b50c6028a4dd9dbf4dff811c51
RyTaus <taus.ryan@gmail.com>
2020-03-22 17:42:04 -0700

    Add comment

--- client/App.tsx
+++ client/App.tsx
@@ -4,7 +4,7 @@ import ApolloClient, {gql} from 'apollo-boost';
 
 export default function App() {
   const client = new ApolloClient({
-    uri: 'http://192.168.86.24:8080/query',
+    uri: 'http://192.168.86.24:8080/query', /* I think the query disapeers once we use UseQuery and UseMutation with react. */
   });
 
   client.query({
